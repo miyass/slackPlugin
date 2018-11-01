837f7069c615c315dd1262d9f294883881f2d8c0
Sohei Miyakura (宮倉 宗平) <s001383@cb.local>
2018-11-01 17:10:32 +0900

    create minimum required function to send slack

--- desktops/desktop.js
+++ desktops/desktop.js
@@ -1,4 +1,24 @@
 (function(PLUGIN_ID) {
     'use strict';
-    
+
+    kintone.events.on('app.record.create.submit', function(event) {
+      var URL = 'https://slack.com/api/chat.postMessage';
+
+      var records = event.record;
+      var channel = records['channel'].value;
+      var text = records['text'].value;
+
+      var body = {
+        "channel": channel,
+        "text": text,
+        "as_user": true
+      }
+
+      kintone.plugin.app.proxy(PLUGIN_ID, URL, 'POST', {}, body, function(body, status, headers) {
+      }, function(error) {
+        console.log(error);
+      });
+      return event;
+    });
+
 })(kintone.$PLUGIN_ID);
