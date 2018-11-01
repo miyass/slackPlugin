f60b59f22caba27038664879fba6e70fd7e3c138
Sohei Miyakura (宮倉 宗平) <s001383@cb.local>
2018-11-01 15:50:19 +0900

    configure first setting screen

--- config_js/config.js
+++ config_js/config.js
@@ -8,5 +8,18 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
+    $('#submit').click(function() {
+      var apitoken = $('#apitoken').val();
+      var headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + apitoken
+      };
+      console.log(headers);
+      kintone.plugin.app.setProxyConfig('https://slack.com/api/chat.postMessage', "POST", headers, {});
+    })
+
+    $('#cancel').click(function() {
+      history.back()
+    });
 
 })(jQuery, kintone.$PLUGIN_ID);
